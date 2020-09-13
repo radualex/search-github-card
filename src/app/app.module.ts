ec99a6d4bd0b7b0857f1ac3c3b27482bfde6f1ed
Radu Stoica <radu.stoica1994@gmail.com>
2020-09-13 21:56:10 +0200

    added search bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { SearchComponent } from "./Search/search.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SearchComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
